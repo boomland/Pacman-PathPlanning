c39f33ab7aa992baee4e3f3cae9323d0ae200959
Ildar <gab.ild@yandex.ru>
2021-05-28 13:01:17 +0300

    Add MCTS

--- pacman_multiagent/multiAgents.py
+++ pacman_multiagent/multiAgents.py
@@ -162,6 +162,35 @@ class MCTSInterface(Agent, object):
             return self._total_reward / self._n_backpropagations
 
         @property
+        def _best_child_UCB(self):
+            best_childs = self._children[0]
+            best_score = None                      
+            for i in self._children:
+                result = (i._total_reward/i._n_backpropagations) + 1*math.sqrt((2*math.log(self._n_backpropagations))/i._n_backpropagations)
+                if result == best_score:
+                    bestChild.append(i)
+                if best_score is None or result > best_score:
+                    best_score = result
+                    bestChild = []
+                    bestChild.append(i)
+            return bestChild[random.randint(0, len(bestChild) - 1)]
+        
+        @property
+        def _best_child_UCT(self):
+            C = math.sqrt(2)
+            best_childs = self._children[0]
+            best_score = None                      
+            for i in self._children:
+                result = (i._total_reward/i._n_backpropagations) + 2*C*math.sqrt((2*math.log(self._n_backpropagations))/i._n_backpropagations)
+                if result == best_score:
+                    bestChild.append(i)
+                if best_score is None or result > best_score:
+                    best_score = result
+                    bestChild = []
+                    bestChild.append(i)
+            return bestChild[random.randint(0, len(bestChild) - 1)]
+
+        @property
         def _best_child(self):
             best_childs = self._children[0]
             best_score = None
@@ -236,6 +265,174 @@ class MCTSAsInHW(MCTSInterface):
                 self._children.append(expanded_child)
                 return expanded_child
             return self
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+    def create_tree(self, game_state):
+        return self.Node(game_state=game_state)
+
+    def simulate(self, game_state):
+        def dist_l1(pos1, pos2):
+            return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+        def find_nearest_food(game_state):
+            curr_pos = game_state.getPacmanPosition()
+            food_grid = game_state.getFood()
+            nearest = None
+            for x in range(food_grid.width):
+                for y in range(food_grid.height):
+                    if not food_grid[x][y]:
+                        continue
+                    if nearest is None or \
+                            dist_l1(curr_pos, (x, y)) < dist_l1(curr_pos, nearest):
+                        nearest = (x, y)
+            return nearest
+
+        nearest_food = None
+        n_iter = 0
+        while not game_state.isWin() and not game_state.isLose() and \
+              n_iter < self._max_simulation_iterations:
+            if nearest_food is None:
+                nearest_food = find_nearest_food(game_state)
+            if nearest_food is None:
+                game_state = game_state.generatePacmanSuccessor(
+                        random.choice(game_state.getLegalActions())
+                )
+            else:
+                best_dist = None
+                best_next_state = None
+                for action in game_state.getLegalActions():
+                    next_state = game_state.generatePacmanSuccessor(action)
+                    curr_dist = dist_l1(next_state.getPacmanPosition(), nearest_food)
+                    if best_dist is None or curr_dist < best_dist:
+                        best_dist = curr_dist
+                        best_next_state = next_state
+                game_state = best_next_state
+                if game_state.getPacmanPosition() == nearest_food:
+                    nearest_food = None
+            n_iter += 1
+        return game_state.getScore()
+
+
+class MCTS_USB(MCTSInterface):
+    """
+      A MCTS agent chooses an action using Monte-Carlo Tree Search.
+    """
+    class Node(MCTSInterface.Node):
+        def __init__(self, game_state, parent=None, action=None):
+            super(self.__class__, self).__init__(game_state, parent, action)
+
+        def select(self):
+            if len(self._possible_actions) != 0 or len(self._children) == 0:
+                return self
+            return self._best_child_UCB.select()
+
+        def expand(self):
+            if len(self._children) != 0 and len(self._possible_actions) == 0:
+                raise Exception("Couldn't select a node without possible children, bit with existing.")
+            while len(self._possible_actions) != 0:
+                action = self._possible_actions[0]
+                self._possible_actions.pop(0)
+                next_game_state = self._game_state.generatePacmanSuccessor(action)
+                if next_game_state.getPacmanPosition() in self._visited_states:
+                    continue
+                expanded_child = self.__class__(
+                    game_state=self._game_state.generatePacmanSuccessor(action),
+                    parent=self,
+                    action=action,
+                )
+                self._children.append(expanded_child)
+                return expanded_child
+            return self
+        
+        #def choose_best_action(self):
+        #    L = [n._total_reward for n in self._children]
+        #    return self._children[L.index(max(L))]._action
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+    def create_tree(self, game_state):
+        return self.Node(game_state=game_state)
+
+    def simulate(self, game_state):
+        def dist_l1(pos1, pos2):
+            return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+        def find_nearest_food(game_state):
+            curr_pos = game_state.getPacmanPosition()
+            food_grid = game_state.getFood()
+            nearest = None
+            for x in range(food_grid.width):
+                for y in range(food_grid.height):
+                    if not food_grid[x][y]:
+                        continue
+                    if nearest is None or \
+                            dist_l1(curr_pos, (x, y)) < dist_l1(curr_pos, nearest):
+                        nearest = (x, y)
+            return nearest
+
+        nearest_food = None
+        n_iter = 0
+        while not game_state.isWin() and not game_state.isLose() and \
+              n_iter < self._max_simulation_iterations:
+            if nearest_food is None:
+                nearest_food = find_nearest_food(game_state)
+            if nearest_food is None:
+                game_state = game_state.generatePacmanSuccessor(
+                        random.choice(game_state.getLegalActions())
+                )
+            else:
+                best_dist = None
+                best_next_state = None
+                for action in game_state.getLegalActions():
+                    next_state = game_state.generatePacmanSuccessor(action)
+                    curr_dist = dist_l1(next_state.getPacmanPosition(), nearest_food)
+                    if best_dist is None or curr_dist < best_dist:
+                        best_dist = curr_dist
+                        best_next_state = next_state
+                game_state = best_next_state
+                if game_state.getPacmanPosition() == nearest_food:
+                    nearest_food = None
+            n_iter += 1
+        return game_state.getScore()
+
+
+class MCTS_UCT(MCTSInterface):
+    """
+      A MCTS agent chooses an action using Monte-Carlo Tree Search.
+    """
+    class Node(MCTSInterface.Node):
+        def __init__(self, game_state, parent=None, action=None):
+            super(self.__class__, self).__init__(game_state, parent, action)
+
+        def select(self):
+            if len(self._possible_actions) != 0 or len(self._children) == 0:
+                return self
+            return self._best_child_UCT.select()
+
+        def expand(self):
+            if len(self._children) != 0 and len(self._possible_actions) == 0:
+                raise Exception("Couldn't select a node without possible children, bit with existing.")
+            while len(self._possible_actions) != 0:
+                action = self._possible_actions[0]
+                self._possible_actions.pop(0)
+                next_game_state = self._game_state.generatePacmanSuccessor(action)
+                if next_game_state.getPacmanPosition() in self._visited_states:
+                    continue
+                expanded_child = self.__class__(
+                    game_state=self._game_state.generatePacmanSuccessor(action),
+                    parent=self,
+                    action=action,
+                )
+                self._children.append(expanded_child)
+                return expanded_child
+            return self
+        
+        #def choose_best_action(self):
+        #    L = [n._total_reward for n in self._children]
+        #    return self._children[L.index(max(L))]._action
 
     def __init__(self):
         super(self.__class__, self).__init__()
